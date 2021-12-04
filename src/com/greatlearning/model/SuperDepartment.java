601d1cc0f428cd461a7d7a6a80fadfae23f17f97
Kungle Raju <kungleraju@gmail.com>
2021-12-05 00:20:28 +0530

    great learning

---
+++ src/com/greatlearning/model/SuperDepartment.java
@@ -0,0 +1,28 @@
+package com.greatlearning.model;
+
+public class SuperDepartment {
+	
+
+
+	public String departmentName() {
+		return "Super Department" ;
+		
+	}
+	
+	
+	public String getTodaysWork() {
+		return "No Work As of now";
+	}
+	
+	public String getWorkDeadline() {
+		return "Nil";
+		
+	}
+	
+	public String isTodayAHoliday() {
+		return "Today is not a holiday ";
+	}
+
+}
+
+
