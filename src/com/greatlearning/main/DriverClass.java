3a7922bd20ed63d96a89ce2a911caa1dcece1c20
Kungle Raju <kungleraju@gmail.com>
2021-12-05 00:15:43 +0530

    Please enter the commit message for your changes. Lines starting
    great learning

---
+++ src/com/greatlearning/main/DriverClass.java
@@ -0,0 +1,53 @@
+package com.greatlearning.main;
+
+import com.greatlearning.model.AdminDepartment;
+import com.greatlearning.model.HrDepartment;
+import com.greatlearning.model.TechDepartment;
+//import com.greatlearning.service.SuperDepartment;
+
+public class DriverClass {
+	
+	public static void main(String[] args)
+	{
+		//SuperDepartment superdepartment = new SuperDepartment();
+		String strWelcome = "Welcome to ";
+		
+		
+		AdminDepartment admindepartment=new AdminDepartment();
+		
+		System.out.println(strWelcome + admindepartment.departmentName());
+		System.out.println(admindepartment.getTodaysWork());
+		System.out.println(admindepartment.getWorkDeadline());
+		System.out.println(admindepartment.isTodayAHoliday());
+		System.out.println();
+		System.out.println();
+
+		
+		
+
+		HrDepartment hrdepartment=new HrDepartment();
+		
+		System.out.println(strWelcome + hrdepartment.departmentName());
+		System.out.println(hrdepartment.doActivity());
+		System.out.println(hrdepartment.getTodaysWork());
+		System.out.println(hrdepartment.getWorkDeadline());
+		System.out.println(hrdepartment.isTodayAHoliday());
+		System.out.println();
+		System.out.println();
+
+		
+		
+		TechDepartment techdepartment=new TechDepartment();
+		
+		System.out.println(strWelcome + techdepartment.departmentName());
+		System.out.println(techdepartment.getTodaysWork());
+		System.out.println(techdepartment.getWorkDeadline());
+		System.out.println(techdepartment.doActivity());
+		System.out.println(techdepartment.isTodayAHoliday());
+		System.out.println();
+		System.out.println();
+
+
+	
+	}
+}
